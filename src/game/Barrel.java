e3408a0637f38db3c041c2adf4f27cdf0a2131dd
Mahan <mahan.otadi@city.ac.uk>
2024-03-06 19:27:09 +0000

    add more barrels to jump

--- src/game/Barrel.java
+++ src/game/Barrel.java
@@ -3,7 +3,10 @@ package game;
 import city.cs.engine.*;
 import org.jbox2d.common.Vec2;
 
-public class Barrel extends StaticBody {
+public class Barrel extends Walker {
+
+    private static int count = 0;
+    private int flag = 0;
     private static final Shape BarrelShape = new CircleShape(3);
 
     private static final BodyImage BarrelImage =
@@ -13,5 +16,10 @@ public class Barrel extends StaticBody {
     public Barrel(World world) {
         super(world, BarrelShape);
         addImage(BarrelImage);
+        count++;
+    }
+
+    public int getCount() {
+        return count;
     }
 }
--- src/game/GameWorld.java
+++ src/game/GameWorld.java
@@ -5,16 +5,24 @@ import org.jbox2d.collision.Collision;
 import org.jbox2d.common.Vec2;
 
 public class GameWorld extends World {
+
+    Barrel[] barrels = new Barrel[10];
+    int x = 260;
     private Runner runner;
 
     public GameWorld(){
         runner = new Runner(this);
         runner.setPosition(new Vec2(-18, -12));
         runner.setGravityScale(0);
-//        runner.startWalking(10);
 
-        Barrel barrel = new Barrel(this);
-        barrel.setPosition(new Vec2(-5,-10));
+        for (int i=0; i<10; i++){
+            barrels[i] = new Barrel(this);
+            barrels[i].setPosition(new Vec2(x - (25 * i),-15));
+            barrels[i].setGravityScale(0);
+            barrels[i].startWalking(-5);
+            barrels[i].setAngularVelocity(-1);
+        }
+
 
     }
 
